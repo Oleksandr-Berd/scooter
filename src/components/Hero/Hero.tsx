2260b0d88dd2aeb3b96840cb03ce4745a9781a5d
Oleksandr-Berd <alex.berd86@gmail.com>
2024-01-12 17:46:18 +0200

    hero home desk is done

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,24 +4,29 @@ import * as SC from './HeroStyled'
 
 import bgMob from '../../assets/images/home-hero-mobile.jpg'
 import bgTab from '../../assets/images/home-hero-tablet.jpg'
+import bgDesk from '../../assets/images/home-hero-desktop.jpg'
 import rightArrow from '../../assets/patterns/right-arrow.svg'
 import circles from '../../assets/patterns/white-circles.svg'
+import line from '../../assets/patterns/line.svg'
 
 import ButtonLink from "../../ui/BtnLink/BtnLink";
 
 const Hero: React.FC = () => {
 
 const isTablet = useMediaQuery("(min-width:768px)")
+const isDesktop = useMediaQuery("(min-width:1440px)")
 
   return (
-    <SC.HeroCon bg={isTablet? bgTab : bgMob} secBg={rightArrow} thirdBg={circles}>
+    <SC.HeroCon bg={isDesktop ? bgDesk : isTablet? bgTab : bgMob} secBg={rightArrow} thirdBg={circles}>
       <SC.HeroTitle>Scooter sharing made simple</SC.HeroTitle>
+      <SC.BodyWrapper bg={line}>
       <SC.HeroBody>
         Scoot takes the hassle out of urban mobility. Our bikes are placed in
         convenient locations in each of our cities. Use our app to locate the
         nearest bike, unlock it with a tap, and you’re away!
       </SC.HeroBody>
       <ButtonLink content="Get Scootin" path="#download"/>
+      </SC.BodyWrapper>
     </SC.HeroCon>
   );
 };
--- src/components/Hero/HeroStyled.ts
+++ src/components/Hero/HeroStyled.ts
@@ -2,25 +2,25 @@ import styled from "@emotion/styled";
 import { IStyles } from "../../utils/interfaces";
 
 export const HeroCon = styled.section<IStyles>`
-position: relative;
+  position: relative;
 
-padding-top: 115px;
-padding-bottom: 179px;
-padding-left: 32px;
-padding-right: 32px;
+  padding-top: 115px;
+  padding-bottom: 179px;
+  padding-left: 32px;
+  padding-right: 32px;
 
-margin-bottom: 120px;
+  margin-bottom: 120px;
 
-text-align: center;
+  text-align: center;
 
-background-image: url(${props => props.bg});
-background-repeat: no-repeat;
-background-size: cover;
+  background-image: url(${(props) => props.bg});
+  background-repeat: no-repeat;
+  background-size: cover;
 
-color: #fff;
+  color: #fff;
 
-&:before{
-    content: url(${props => props.secBg});
+  &:before {
+    content: url(${(props) => props.secBg});
 
     position: absolute;
 
@@ -28,9 +28,9 @@ color: #fff;
     left: 0;
 
     transform: translateX(-312px) translateY(-34px);
-}
+  }
 
-@media (min-width: 768px){
+  @media (min-width: 768px) {
     padding-top: 137px;
     padding-bottom: 217px;
     padding-left: 97px;
@@ -38,42 +38,93 @@ color: #fff;
 
     margin-bottom: 122px;
 
-    &:before{
-        transform: translateX(-127px) translateY(-34px);
+    &:before {
+      transform: translateX(-127px) translateY(-34px);
     }
 
-    &:after{
-        content: url(${props => props.thirdBg});
+    &:after {
+      content: url(${(props) => props.thirdBg});
 
-        position: absolute;
-        bottom: 0;
-        right: 0;
+      position: absolute;
+      bottom: 0;
+      right: 0;
 
-        transform: translateX(31px) translateY(-45px);
+      transform: translateX(31px) translateY(-45px);
     }
-}
-`
+  }
+
+  @media (min-width: 1440px) {
+    padding-top: 153px;
+    padding-bottom: 152px;
+    padding-left: 165px;
+
+    text-align: left;
+
+    &:before {
+      transform: translateX(705px) translateY(-145px);
+    }
+
+    &:after {
+      content: url(${(props) => props.thirdBg});
+
+      position: absolute;
+      bottom: 0;
+      right: 0;
+
+      transform: translateX(31px) translateY(-165px);
+    }
+  }
+`;
 
 export const HeroTitle = styled.h2`
-margin-bottom: 24px;
+  margin-bottom: 24px;
 
-font-size: 40px;
-letter-spacing: -1.79px;
+  font-size: 40px;
+  letter-spacing: -1.79px;
 
-@media (min-width: 768px){
+  @media (min-width: 768px) {
     font-size: 56px;
     line-height: 1;
     letter-spacing: -2.5px;
-}
-`
+  }
+
+  @media (min-width: 1440px) {
+    width: 500px;
+
+    margin-bottom: 40px;
+  }
+`;
+
+export const BodyWrapper = styled.div<Partial <IStyles>>`
+  @media (min-width: 1440px) {
+    position: relative;
+
+    width: 405px;
+
+    margin-left: calc(260px - 165px);
+
+    &::before{
+        content: url(${props => props.bg});
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        transform: translateX(-260px) translateY(50px);
+    }
+  }
+`;
 
 export const HeroBody = styled.p`
-margin-bottom: 34px;
+  margin-bottom: 34px;
 
-font-size: 15px;
-line-height: 1.67;
+  font-size: 15px;
+  line-height: 1.67;
 
-@media (min-width: 768px){
+  @media (min-width: 768px) {
     margin-bottom: 32px;
-}
-`
+  }
+
+  @media (min-width: 1440px) {
+    margin-bottom: 40px;
+  }
+`;
